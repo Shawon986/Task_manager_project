5baac6cd3da776d72cb6ba06c2e53d39b27f4313
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-08-03 22:00:48 +0600

    add authHeader validator

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,10 +3,14 @@ const jwt = require("jsonwebtoken");
 //Middleware
 module.exports= function (req, res, next) {
   const authHeader = req.headers.authorization;
+  if(!authHeader){
+    res.status(500).json({message:"AuthHeader is not found"});
+    return
+  }
   const accessToken = authHeader && authHeader.split(" ")[1];
   if (!accessToken) {
     res.status(401).json({ message: "Visitor unauthorized" });
-    return;
+    return
   } else {
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, payload) => {
       if (err) {
