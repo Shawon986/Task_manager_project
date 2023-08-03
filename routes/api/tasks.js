aa7fe3bbc170844456a288d7f219cc8a39b4bc2b
Shawon Hossain <mdmonowerhossain986@gmail.com>
2023-08-03 22:33:21 +0600

    update a task by visitor api done

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -6,16 +6,15 @@ const authAccessToken = require("../../middleware/auth");
 const router = express.Router();
 const Task = require("../../models/task");
 
-router.post("/newTask",[authAccessToken], async (req, res) => {
+router.post("/newTask", [authAccessToken], async (req, res) => {
   try {
-    const id = req.payload.id
+    const id = req.payload.id;
     const taskObject = {
       title: req.body.title,
       desc: req.body.desc ?? "",
-      userId :id,
-      
+      userId: id,
     };
-    const task = new Task(taskObject); 
+    const task = new Task(taskObject);
     res.status(201).json(task);
     await task.save();
   } catch (error) {
@@ -26,173 +25,161 @@ router.post("/newTask",[authAccessToken], async (req, res) => {
   }
 });
 
+//! Get all tasks by visitors
+router.get("/get", authAccessToken, async (req, res) => {
+  try {
+    const id = req.payload.id;
+    const task = await Task.find({ userId: id });
+    res.json(task);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .json({ message: "Something went wrong with the server !!!" });
+  }
+});
 
-  //! Get all tasks by visitors
-  router.get("/get",authAccessToken, async (req, res) => {
-    try {
-      const id = req.payload.id
-      const task = await Task.find({userId:id});
+//! Update a task by visitor
+router.put("/update/:id", authAccessToken, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!task) {
+      res.status(404).json({ message: "task not found" });
+    } else {
       res.json(task);
-    } catch (error) {
-      console.error(error);
-      res
-        .status(400)
-        .json({ message: "Something went wrong with the server !!!" });
+      await task.save();
     }
-  });
+  } catch (error) {}
+});
 
-  //! Get a visitor by id
+//! Get a visitor by id
 router.get("/:id", async (req, res) => {
-    try {
-      const id = req.params.id;
-      const visitor = await Visitors.findById(id);
-      if (!visitor) {
-        res.status(404).json({ message: "Visitor not found" });
-      } else {
-        res.json(visitor);
-      }
-    } catch (error) {
-      console.error(error);
-      res
-        .status(400)
-        .json({ message: "Something went wrong with the server !!!" });
+  try {
+    const id = req.params.id;
+    const visitor = await Visitors.findById(id);
+    if (!visitor) {
+      res.status(404).json({ message: "Visitor not found" });
+    } else {
+      res.json(visitor);
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .json({ message: "Something went wrong with the server !!!" });
+  }
+});
 
-  //! Update a visitor by id
-router.put("/:id", async (req, res) => {
-    try {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPass = await bcrypt.hash(req.body.password, salt);
-      const id = req.params.id;
-      const visitor = await Visitors.findByIdAndUpdate(id, req.body, {
-        new: true,
-      });
-      if (!visitor) {
-        res.status(404).json({ message: "Visitor not found" });
-      } else {
-        visitor.password = hashedPass;
-        res.json(visitor);
-        await visitor.save();
-      }
-    } catch (error) {}
-  });
-  
-  //! Delete a visitor by id
-  router.delete("/:id", async (req, res) => {
-    try {
-      const id = req.params.id;
-      const visitor = await Visitors.findByIdAndDelete(id);
-      if (!visitor) {
-        res.status(404).json({ message: "Visitor not found" });
-      } else {
-        res.json(visitor);
-      }
-    } catch (error) {}
-  });
-  
-  //Login a visitor
+//! Delete a visitor by id
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const visitor = await Visitors.findByIdAndDelete(id);
+    if (!visitor) {
+      res.status(404).json({ message: "Visitor not found" });
+    } else {
+      res.json(visitor);
+    }
+  } catch (error) {}
+});
+
+//Login a visitor
 
 router.post("/login", async (req, res) => {
-    try {
-      const { email, password, type, refreshToken } = req.body;
-      if (!type) {
-        res.status(401).json({ message: "Type is not defined" });
-      } else {
-        if (type == "email") {
-          await emailLogin(email, res, password);
-        } else {
-          refreshTokenLogin(refreshToken, res, password);
-        }
-      }
-    } catch (error) {
-      console.error(error);
-      res
-        .status(400)
-        .json({ message: "Something went wrong with the server !!!" });
-    }
-  });
-  //! Get a visitor profile
-  router.get("/user/profile", authAccessToken, async (req, res) => {
-    try {
-      const id = req.payload.id;
-      const visitor = await Visitors.findById(id);
-      if (!visitor) {
-        res.status(404).json({ message: "Visitor  not found" });
-      } else {
-        res.json(visitor);
-      }
-    } catch (error) {
-      console.error(error);
-      res
-        .status(400)
-        .json({ message: "Something went wrong with the server !!!" });
-    }
-  });
-
-  module.exports = router
-
-  function refreshTokenLogin(refreshToken, res, password) {
-    if (!refreshToken) {
-      res.status(401).json({ message: "RefreshToken is not defined" });
+  try {
+    const { email, password, type, refreshToken } = req.body;
+    if (!type) {
+      res.status(401).json({ message: "Type is not defined" });
     } else {
-      jwt.verify(
-        refreshToken,
-        process.env.JWT_SECRET,
-        async (err, payload) => {
-          if (err) {
-            res.status(401).json({ message: "Visitor unauthorized" });
-          } else {
-            const id = payload.id;
-            const visitor = await Visitors.findById(id);
-            if (!visitor) {
-              res.status(404).json({ message: "Visitor  not found" });
-            } else {
-              const passvalidity = await bcrypt.compare(
-                password,
-                visitor.password
-              );
-              if (!passvalidity) {
-                res.status(401).json({ message: "Visitor unauthorized" });
-              } else {
-                getVisitorToken(visitor, res);
-              }
-            }
-          }
-        }
-      );
+      if (type == "email") {
+        await emailLogin(email, res, password);
+      } else {
+        refreshTokenLogin(refreshToken, res, password);
+      }
     }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .json({ message: "Something went wrong with the server !!!" });
   }
-  
-  async function emailLogin(email, res, password) {
-    const visitor = await Visitors.findOne({ email: email });
+});
+//! Get a visitor profile
+router.get("/user/profile", authAccessToken, async (req, res) => {
+  try {
+    const id = req.payload.id;
+    const visitor = await Visitors.findById(id);
     if (!visitor) {
       res.status(404).json({ message: "Visitor  not found" });
     } else {
-      const passvalidity = await bcrypt.compare(password, visitor.password);
-      if (!passvalidity) {
+      res.json(visitor);
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .json({ message: "Something went wrong with the server !!!" });
+  }
+});
+
+module.exports = router;
+
+function refreshTokenLogin(refreshToken, res, password) {
+  if (!refreshToken) {
+    res.status(401).json({ message: "RefreshToken is not defined" });
+  } else {
+    jwt.verify(refreshToken, process.env.JWT_SECRET, async (err, payload) => {
+      if (err) {
         res.status(401).json({ message: "Visitor unauthorized" });
       } else {
-        getVisitorToken(visitor, res);
+        const id = payload.id;
+        const visitor = await Visitors.findById(id);
+        if (!visitor) {
+          res.status(404).json({ message: "Visitor  not found" });
+        } else {
+          const passvalidity = await bcrypt.compare(password, visitor.password);
+          if (!passvalidity) {
+            res.status(401).json({ message: "Visitor unauthorized" });
+          } else {
+            getVisitorToken(visitor, res);
+          }
+        }
       }
+    });
+  }
+}
+
+async function emailLogin(email, res, password) {
+  const visitor = await Visitors.findOne({ email: email });
+  if (!visitor) {
+    res.status(404).json({ message: "Visitor  not found" });
+  } else {
+    const passvalidity = await bcrypt.compare(password, visitor.password);
+    if (!passvalidity) {
+      res.status(401).json({ message: "Visitor unauthorized" });
+    } else {
+      getVisitorToken(visitor, res);
     }
   }
-  
-  function getVisitorToken(visitor, res) {
-    const accessToken = jwt.sign(
-      { email: visitor.email, id: visitor._id },
-      process.env.JWT_SECRET
-    );
-    const refreshToken = jwt.sign(
-      { email: visitor.email, id: visitor._id },
-      process.env.JWT_SECRET
-    );
-  
-    visitorObject = visitor.toJSON();
-  
-    visitorObject.accessToken = accessToken;
-    visitorObject.refreshToken = refreshToken;
-  
-    res.json(visitorObject);
-  }
-  
+}
+
+function getVisitorToken(visitor, res) {
+  const accessToken = jwt.sign(
+    { email: visitor.email, id: visitor._id },
+    process.env.JWT_SECRET
+  );
+  const refreshToken = jwt.sign(
+    { email: visitor.email, id: visitor._id },
+    process.env.JWT_SECRET
+  );
+
+  visitorObject = visitor.toJSON();
+
+  visitorObject.accessToken = accessToken;
+  visitorObject.refreshToken = refreshToken;
+
+  res.json(visitorObject);
+}
